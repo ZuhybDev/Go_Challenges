178b977397538b0b12a7a8c5bcce54daa5f33cad
ZuhybDev <msuhaib637@gmail.com>
2026-01-09 11:37:34 +0300

    easy challenge

---
+++ main.js
@@ -0,0 +1,11 @@
+function anacondaReticle(name, length, color, action) {
+  // Write code here
+  console.log("[RETICLE ACTIVE]");
+  console.log("Subject: Anaconda");
+  console.log("Name: ", name);
+  console.log("Length: ", length);
+  console.log("Color: ", color);
+  console.log("Activity: ", action);
+}
+
+console.log(anacondaReticle("Python", "1.4", "red", "resting"));
