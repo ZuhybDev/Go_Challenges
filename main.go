5f2e80014c5cb74f6816b0e3e7eba07274b1fbcc
Zuhaib Mohammed <msuhaib637@gmail.com>
2026-02-03 21:40:40 +0300

    Campus Move Scheduling

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
-import "fmt"
-
-func formatAppointment(patientName string, appointmentTime string) string {
-
-	return fmt.Sprintf("Patient: %s - Appointment: %s\n", patientName, appointmentTime)
-}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	formatAppointment("", "2:30 PM")
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Server starts at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+
+	path := r.URL.Path
+	query := r.URL.Query().Get("id")
+
+	fmt.Fprintf(w, "You sent a %s request to %s with ID %s\n",
+		method, path, query)
 }
