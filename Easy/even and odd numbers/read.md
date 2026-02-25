4641557daa6b41d884f68be1637907390978a82f
zuhaibdev <msuhaib637@gmail.com>
2026-02-25 17:33:31 +0300

    easy

---
+++ Easy/even and odd numbers/read.md
@@ -0,0 +1,5 @@
+We have an array like this:
+
+[20, 6, 78, 89, 9, 12, 33]
+
+Write a function that takes an array of numbers and performs multiplication for the odd numbers and addition for the even numbers, returning the sum of them (sumOfEven + sumOfOdd ).
--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import "fmt"
 
+func performOperations(nums []int) {
+
+	result := 0
+	sumOfEven := 1
+	sumOfOdd := 1
+
+	for _, n := range nums {
+
+		if n%2 == 0 {
+			sumOfEven *= n
+		} else {
+			sumOfOdd *= n
+		}
+
+	}
+
+	result = sumOfEven + sumOfOdd
+
+	fmt.Println(result)
+}
+
 func main() {
-	// Example with an odd number of elements
-	oddArray := []int{100, 4, 6, 45, 8, 8, 67}
-	middleIndexOdd := len(oddArray) / 2
-	middleElementOdd := oddArray[middleIndexOdd]
-	fmt.Printf("Odd length array: %v\n", oddArray)
-	fmt.Printf("Middle element (index %d): %d\n\n", middleIndexOdd, middleElementOdd)
 
-	// Example with an even number of elements
-	evenSlice := []string{"apple", "banana", "cherry", "date"}
-	middleIndexEven := len(evenSlice) / 2 // 4 / 2 = 2
+	nums := []int{20, 67, 456, 89, 9, 122, 33}
+	// nums := []int{1, 2, 3, 4, 5, 6}
 
-	fmt.Printf("Even length slice: %v\n", evenSlice)
-
-	// For an even length, you may consider one or both of the two middle elements
-	// The index calculation gives the second of the two middle elements
-	middleElementEven1 := evenSlice[middleIndexEven-1]
-	middleElementEven2 := evenSlice[middleIndexEven]
-	fmt.Printf("Middle elements (indices %d and %d): %s and %s\n", middleIndexEven-1, middleIndexEven, middleElementEven1, middleElementEven2)
+	performOperations(nums)
 }
